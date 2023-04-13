8f6c4f3df12a6505b6ccafed2ae35fd0d1cea4cc
Phillip Stewart <phstewart@microsoft.com>
2023-04-13 10:58:26 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Home';
+import Test from './Test';
 
 function App() {
-  const value = 'World, Part 2!';
-  return <div>Hello {value}</div>;
+    return (
+    <Router>
+        <Routes >
+            <Route path="/" element={<Test />} />
+            <Route path="/home" element={<Home />} />
+        </Routes>
+    </Router>
+    );
 }
 
 export default App;
